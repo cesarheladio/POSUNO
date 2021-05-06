c8e1f2b56110791e6a7336d590d62c6164b31685
cesarheladio <47466224+cesarheladio@users.noreply.github.com>
2021-05-06 13:09:14 -0500

    finish video 4

---
+++ POSUNO/POSUNO.Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using POSUNO.Helpers;
+using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace POSUNO.Pages
+{   
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private  async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = await ValidForm();
+            if(!isValid)
+            {
+                return;
+            }
+            MessageDialog messageDialog = new MessageDialog("bien","ok");
+            await messageDialog.ShowAsync();
+        }
+
+        private async Task<bool> ValidForm()
+        {
+            MessageDialog messageDialog;
+
+            if (string.IsNullOrEmpty(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+            if (!RegexUtilities.IsValidEmail(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar un email valido.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+            if (string.IsNullOrEmpty(PasswordPasswordBox.Password))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu contraseña.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+            return true;
+        }
+    }
+}
